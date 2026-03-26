a5c8f07c5b3f1b0ef05364489064cf44523c2dad
haloactief <artemmanushkin@outlook.com>
2026-03-26 19:59:38 +0700

    HAHAHAHA IT WORKS

--- html/script.js
+++ html/script.js
@@ -1,41 +1,47 @@
 import { joinRoom, selfId } from 'https://esm.run/@trystero-p2p/torrent';
 // import WebTorrent from 'https://esm.sh/webtorrent/dist/webtorrent.min.js';
 
-// store bounties in local storage
+// store tasks in local storage
 // and broadcast them to all peers
 // im fucking genius
 
-const bountyContainer = document.getElementById("bounties");
-var myBounties = localStorage.getItem("mb") !== null ? JSON.parse(localStorage.getItem("mb")) : [];
-var otherBounties = [];
+const newPeerSpan = document.getElementById("new-peer");
+const tasksContainer = document.getElementById("tasks");
+const chat = document.getElementById("chat");
+var myTasks = localStorage.getItem("mt") !== null ? JSON.parse(localStorage.getItem("mt")) : [];
+var otherTasks = [];
 
-const updateBountyContainer = () => {
-  const bounties = [...myBounties, ...otherBounties];
-  bountyContainer.innerHTML = "";
-  bounties.forEach(obj => {
-    const bounty = document.createElement("div");
-    const bountyHeader = document.createElement("span");
-    bountyHeader.textContent = obj.header;
+const updateTaskContainer = () => {
+  const tasks = [...myTasks, ...otherTasks];
+  tasksContainer.innerHTML = "<h1>Tasks</h1>";
+  tasks.forEach(obj => {
+    const task = document.createElement("div");
+    const taskHeader = document.createElement("h1");
+    taskHeader.textContent = obj.header;
 
-    const bountyBody = document.createElement("p");
-    bountyBody.textContent = obj.body;
+    const taskDesc = document.createElement("p");
+    taskDesc.textContent = obj.body;
 
-    bounty.appendChild(bountyHeader);
-    bounty.appendChild(bountyBody);
+    task.appendChild(taskHeader);
+    task.appendChild(taskDesc);
 
-    bountyContainer.appendChild(bounty);
+    const peer = document.createElement("span");
+    peer.textContent = obj.peerId ? `peer: ${obj.peerId}` : `peer: me`;
+    task.appendChild(peer);
+
+    tasksContainer.appendChild(task);
   })
 }
 
-updateBountyContainer(myBounties);
+updateTaskContainer();
 
 const config = {
   appId: 'abebe',
   relayUrls: [`wss://domain/ws/`] // <-- change_domain
   // rtcConfig: {
   //   iceServers: [
-  //     {
-  //       urls: `turn:domain:3478`, // <-- change_domain
+  //     {  // need this if p2p fails
+  //       urls: `turn:domain:3478`, //  <-- change_domain
   //       username: "stupidboy",
   //       credential: "hardpassword"
   //     }
@@ -43,33 +49,49 @@ const config = {
   // }
 };
 const room = joinRoom(config, 'yoyo');
-console.log(`id: ${selfId}`);
+document.getElementById("selfId").innerText = `your id: ${selfId}`;
 
 const [sendM, getM] = room.makeAction('chat')
 
 room.onPeerJoin(peerId => {
-  console.log(`new peer: ${peerId}`);
-  sendBounties();
+  newPeerSpan.textContent = newPeerSpan.textContent + ` new peer: ${peerId}`;
+  setTimeout(() => {
+    newPeerSpan.style.display = "none";
+  }, 5000);
+  sendMyTasks();
 })
 
 const sendMessage = () => {
-  sendM({type: "gm", body: document.getElementById("text").value});
+  const recipientId = document.getElementById("recipient").value;
+  const message = document.getElementById("message").value;
+  if(recipientId) {
+    sendM({type: "m", mtype: "private", body: message}, recipientId);
+  } else {
+    sendM({type: "m", mtype: "global", body: message});
+  }
+
+  const mdiv = document.createElement("div");
+  mdiv.textContent = recipientId ? `you to ${recipientId}: ${message} ${Date.now()}` : `you to all: ${message} ${Date.now()}`;
+  chat.appendChild(mdiv);
 }
 document.getElementById("send-button").addEventListener("click", sendMessage);
 
 getM((data, peerId) => {
   switch(data.type) {
   case "b":
-    data.body.forEach(bounty => {
-      const exist = otherBounties.some(obj => obj.header === bounty.header);
+    data.body.forEach(task => {
+      const exist = otherTasks.some(obj => obj.header === task.header);
       if(!exist) {
-        otherBounties.push(bounty);
+        const taskWPeer = {...task, peerId: data.peerId};
+        otherTasks.push(taskWPeer);
       }
-      updateBountyContainer();
+      updateTaskContainer();
     })
     break;
   case "m":
-    console.log(`${peerId}: ${data.body} ${Date.now()} ${data.type}`);
+    const message = document.createElement("div");
+    message.textContent = data.mtype === "private" ? `${peerId} to you: ${data.body} ${Date.now()}` : `${peerId} to all: ${data.body} ${Date.now()}`;
+    chat.appendChild(message);
     break;
   default:
     console.log("unknown message type");
@@ -77,25 +99,25 @@ getM((data, peerId) => {
   }
 })
 
-const createBounty = () => {
-  const bountyHeader = document.getElementById("bounty-header").value;
-  const bountyBody = document.getElementById("bounty-body").value;
+const createTask = () => {
+  const taskHeader = document.getElementById("task-header").value;
+  const taskBody = document.getElementById("task-desc").value;
 
-  const exists = myBounties.some(obj => obj.header === bountyHeader);
+  const exists = myTasks.some(obj => obj.header === taskHeader);
   if (exists) return;
 
-  const bountyObj = {
-    header: bountyHeader,
-    body: bountyBody
+  const taskObj = {
+    header: taskHeader,
+    body: taskBody
   };
-  myBounties.push(bountyObj);
-  localStorage.setItem("mb", JSON.stringify(myBounties));
-  sendBounties();
-  updateBountyContainer();
+  myTasks.push(taskObj);
+  localStorage.setItem("mt", JSON.stringify(myTasks));
+  sendMyTasks();
+  updateTaskContainer();
 }
 
-const sendBounties = () => {
-  sendM({type: "b", body: myBounties, peerId: selfId});
+const sendMyTasks = () => {
+  sendM({type: "b", mtype: "global", body: myTasks, peerId: selfId});
 }
 
-document.getElementById("bounty-button").addEventListener("click", createBounty);
+document.getElementById("task-button").addEventListener("click", createTask);
